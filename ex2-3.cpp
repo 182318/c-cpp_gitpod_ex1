5db8afdfd9d55bbcf0aae046b27df9990a67f8a3
182318 <104435376+182318@users.noreply.github.com>
2022-05-03 23:09:31 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int a,b;
+
+int main() {
+  
+  cout << "Enter two number: " ;
+
+  scanf("%d", &a);
+  scanf("%d", &b);
+
+  cout << "the square of the two number = " << a*a << "\t" << b*b;
+    
+  return 0;
+}
