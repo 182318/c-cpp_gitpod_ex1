218db351e6f86ad15a64f1e22d416bec4791d107
182318 <104435376+182318@users.noreply.github.com>
2022-05-04 16:23:01 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main (void) {
+     printf(" hello Selena "); 
+     printf(" my height is %d m %.2lf cm \n",1,75.7575);
+     printf(" this building is %10d cm \n", 890);
+     printf(" this building is %10d cm \n", 7890);
+     printf(" this building is %10d cm \n", 67890);
+     printf(" this building is %10d cm \n", 567890);
+     printf(" this building is %d cm \n", 1234567890);
+     printf(" pi = %.7lf \n ", 3.1415926535897932384626433832795);
+}
