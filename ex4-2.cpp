b225c9bbbba67169f04da6baea093c4856716d93
182318 <104435376+182318@users.noreply.github.com>
2022-05-17 21:54:45 +0800

    Create ex4-2.cpp

---
+++ ex4-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    int x,y,z;
+    int bool_value;
+     
+    printf("please input x:\n");
+    scanf("%d",&x);
+    printf("please input y:\n");
+    scanf("%d",&y);
+    
+    if(x>y){
+        printf("x>y ==>%d \n",x>y);
+    }else if(x<y) {
+        printf("x<y ==>%d \n",x>y);
+    }else {
+        printf("x==y ==%d \n",x==y);
+    }
+    
+    z=(x>y)?x:y;
+    printf("z=%d \n",z);
+
+    return 0;
+}
